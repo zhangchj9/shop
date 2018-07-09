675b106e714fca948be4f884f89fc243fbaeff12
zhangchj9 <zhangchj9@mail2.sysu.edu.cn>
2018-07-09 03:49:39 +0000

    final version v2.0

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,5 +11,6 @@ class DatabaseSeeder extends Seeder
         $this->call(ProductsSeeder::class);
         $this->call(CouponCodesSeeder::class);
         $this->call(OrdersSeeder::class);
+        $this->call(UsersTableSeeder::class);
     }
 }
